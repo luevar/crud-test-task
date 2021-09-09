1d5ed5c390d3ba486555931e335edb0ec98a2707
luevar <velikasov.alexander@gmail.com>
2021-09-09 23:58:23 +0300

    initial commit

---
+++ src/main/java/gaz/crudtesttask/CrudTestTaskApplication.java
@@ -0,0 +1,13 @@
+package gaz.crudtesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gaz/crudtesttask/CrudTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package gaz.crudtesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
