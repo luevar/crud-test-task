8bb43e39a6249629331626148b8db467d43761dc
luevar <velikasov.alexander@gmail.com>
2021-09-10 00:52:06 +0300

    Quick fix

--- src/main/java/gaz/crudtesttask/exceptions/UserNotFoundException.java
+++ src/main/java/gaz/crudtesttask/exceptions/UserNotFoundException.java
@@ -3,6 +3,6 @@ package gaz.crudtesttask.exceptions;
 public class UserNotFoundException extends RuntimeException{
 
     public UserNotFoundException(Long id) {
-        super("Can not find user " + id);
+        super("Can not find user " + id + "\n");
     }
 }
